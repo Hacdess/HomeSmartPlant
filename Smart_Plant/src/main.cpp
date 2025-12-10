d602fc784292da56f45d12d638b1ad9a11460cef
AnhTtis <nguyenhuuanhtri866@gmail.com>
2025-12-11 01:00:35 +0700

    Fixing the code to successfully connect with MQTT

--- Smart_Plant/src/main.cpp
+++ Smart_Plant/src/main.cpp
@@ -29,14 +29,14 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // =======================================================
 //                     WIFI & MQTT CONFIG
 // =======================================================
-const char* ssid = "YOUR_WIFI";
-const char* password = "YOUR_PASSWORD";
+const char* ssid = "";
+const char* password = "";
 
 const char* mqttServer = "c91c3b64d47444098772381daeb628ea.s1.eu.hivemq.cloud";
 const int mqttPort = 8883;
 
-const char* mqttUser = "mqtt-user";
-const char* mqttPass = "mqtt-password";
+const char* mqttUser = "SmartPlant";
+const char* mqttPass = "My_plant@123";
 
 const char* deviceID = "esp32_c91c3b64d47444098772381daeb628ea";
 
@@ -45,21 +45,35 @@ const char* deviceID = "esp32_c91c3b64d47444098772381daeb628ea";
 // =======================================================
 const char* root_ca =
 "-----BEGIN CERTIFICATE-----\n"
-"MIIDSjCCAjKgAwIBAgIQRl5NvKPC3DxbhQ36SEs88TANBgkqhkiG9w0BAQsFADA/\n"
-"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFjAUBgNVBAMT\n"
-"DURTVCBSb290IENBIFgzMB4XDTIwMDkwMTAwMDAwMFoXDTMwMDgyOTIzNTk1OVow\n"
-"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRYwFAYDVQQD\n"
-"Ew1EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n"
-"AJ0T0U/3sSVDQKp0qKXqQyV2pAonw8rWBVg1SPjz67sV1Nw3pS4o0lS2Vx2jcWc1\n"
-"hlV5PxL2M8xSD1XD4kvp+JaR6R1x7U0EdGDQmL1o0jXP9DD35JtIhXCTITVEWilR\n"
-"vJ6Z0TiQ2jHGhEq+y3OC/YLXTr4Wr9yi5xRngk6VwBEmc6k6R/+qvOB0fOkH1ZZ1\n"
-"p1Zd0NenE2x0RvrRZtGJPD7W82dManIeZDV4mQdlqzTeWY5AvzkdxlIo0NGdiszI\n"
-"gxF5EYh3YOEoTP0TO+GfY+3CX6XfGZV8oPZxC25f6/sOlne342XQI3/OQvPsvPV/\n"
-"Fnf4Idu8LojRxurx8Wc58SUCAwEAAaNCMEAwDgYDVR0PAQH/BAQDAgEGMA8GA1Ud\n"
-"EwEB/wQFMAMBAf8wHQYDVR0OBBYEFMSmc5QwTqHoPazTA/gkGEXUX/MmMA0GCSqG\n"
-"SIb3DQEBCwUAA4IBAQC24HGIcG9H9n1tFoZT3zh0+BTtPlqvGjufH6G+jD/adJzi\n"
-"UTduVdJN9WewtG/XAIN5e8w1sM+d1ZBgU984wgKqeFTig84yYI6FqdtYY4c3SeN5\n"
-"lSRXyoxkBq/Y7W82dManIeZDV4mQdlqzTeWY5AvzkdxlIo0NGdiszI+QwaP42Qik\n"
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n"
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n"
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n"
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n"
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n"
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n"
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n"
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n"
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n"
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n"
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n"
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n"
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n"
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n"
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n"
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n"
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n"
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n"
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n"
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n"
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n"
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n"
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n"
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n"
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
 "-----END CERTIFICATE-----\n";
 
 // =======================================================
@@ -187,7 +201,7 @@ void sendSensorsMQTT() {
 //                        SETUP
 // =======================================================
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   dht.begin();
   lcd.init();
